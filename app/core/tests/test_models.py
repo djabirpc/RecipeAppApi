e473de5bf69dbefa4360946427725098739ebe98
djabirpc <djabir.pc@outlook.fr>
2021-11-21 14:30:13 +0100

    Added Ingredients feature.

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -50,3 +50,12 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(tag), tag.name)
+    
+    def test_ingredient_str(self):
+        """Test the ingedient string representation"""
+        ingredient = models.Ingredient.objects.create(
+            user = sample_user(),
+            name = 'Cucumber'
+        )
+
+        self.assertEqual(str(ingredient),ingredient.name)
